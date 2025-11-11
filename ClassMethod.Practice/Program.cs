9920f521664bca0bff422a5c17ae2cac4ff2e7d3
zarifatb-15 <zarifatb@code.edu.az>
2025-11-11 09:03:43 +0400

    added task

--- ClassMethod.Practice/Program.cs
+++ ClassMethod.Practice/Program.cs
@@ -28,7 +28,24 @@ class Program
         // Console.Write("Eded daxil edin:");
         // int number=int.Parse(Console.ReadLine());
         // IsPrimeOrComposite(number);
-        
+        // Console.Write("number ededini daxil edin:");
+        // int number = int.Parse(Console.ReadLine());
+        // Console.Write("number2 ededini daxil edin:");
+        // int number2 = int.Parse(Console.ReadLine());
+        // Console.WriteLine($"{number} ve {number2} araligindaki polindrom ededler:");
+        // IsPolindrom(number, number2);
+        // string text = "babayeva";
+        // RepeatedChar(text);
+       // double result= CheckTriangle(3,4,5);
+       // Console.WriteLine($"sumOfSquares: {result}");
+       // string text = GetMiddleChar("armud");
+       // string text2 = GetMiddleChar("portagal");
+       // Console.WriteLine($"{text} {text2}");
+
+
+
+
+
 
 
 
@@ -39,7 +56,7 @@ class Program
         int sum = 0;
         foreach (int num in array)
                      sum += num;
-                     return sum;
+        return sum;
     }
 
     static string RemoveStringFromString(string word)
@@ -70,7 +87,7 @@ class Program
         int result = 1;
         for (int i=1; i<=num; i++)
             result *= i;
-            return result;
+        return result;
     }
 
     public static int[] FactorialArray(int[] array)
@@ -153,8 +170,88 @@ class Program
         Console.WriteLine($"{num} sade ededdir");
     }
     // TASK 5
-    
-    
-    
+    public static void IsPolindrom(int number ,int number2)
+    {
+        for (int i = number; i <=number2; i++)
+        {
+            int original = i;
+            int reverse = 0;
+            int num = i;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                reverse = reverse * 10 + digit;
+                num /= 10;
+            }
+            if (reverse == original) 
+                Console.WriteLine(original);
+        }
+    }
+    // TASK 6
+    public static void RepeatedChar(string text)
+    {
+        Dictionary<char, int> charCount = new Dictionary<char, int>();
+        foreach (char c in text)
+        {
+            if (charCount.ContainsKey(c))
+                charCount[c]++;
+            else
+                charCount[c] = 1;
+        }
+
+        char bigchar =' ';
+        int bigcount = 0;
+        foreach (var item in charCount)
+        {
+            if (item.Value > bigcount)
+            {
+                bigcount = item.Value;
+                bigchar = item.Key;
+            }
+        }
+
+        Console.WriteLine($"En cox tekrarlanan simvol: {bigchar}, tekrar say: {bigcount}");
+    }
+    // TASK7
+    public static double CheckTriangle(double a, double b, double c)
+    {
+        if (a + b <= c || a + c <= b || b + c <= a)
+        {
+            Console.WriteLine("Ucbucaq deyil");
+            return 0;
+        }
+        if(a==b && b==c)
+            Console.WriteLine("Beraber terefli ucbucaq");
+        else if (a == b || b == c || a == c)
+            Console.WriteLine("Ikiterefli ucbucaq");
+        else 
+            Console.WriteLine("Beraber terefli ucbucaq deyil");
+
+        double maxSide = a;
+        if(b>maxSide) maxSide = b;
+        if(c>maxSide) maxSide = c;
+        
+        double sumOfSquares = a*a+b*b+c*c- maxSide*maxSide;
+        if (Math.Abs(maxSide * maxSide - sumOfSquares) < 0.0001)
+        {
+            Console.WriteLine("Duzbucaqli ucbucaqdi");
+        }
+        else
+        {
+            Console.WriteLine( "Duzbucaqli ucbucaq deyil");
+        }
+           return sumOfSquares;
+    }
+    // TASK 8
+    public static string GetMiddleChar(string str)
+    {
+        int lenght=str.Length;
+        int middle=lenght / 2;
+        if(lenght % 2 == 0) 
+            return $"{str[middle - 1]}{str[middle]}";
+        else
+            return str[middle].ToString();
+    }
+    // TASK 9
     
 }
